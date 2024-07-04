0b3cd1102a0412ba8f7cce9e8489a08d168c2cb6
Inho Oh <inho.oh@sk.com>
2024-07-04 17:37:06 +0900

    fix portfile
    Signed-off-by: Inho Oh <inho.oh@sk.com>

--- ports/aln/portfile.cmake
+++ ports/aln/portfile.cmake
@@ -6,12 +6,18 @@ vcpkg_from_github(
 	HEAD_REF master
 )
 
-vcpkg_cmake_configure()
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+)
+
 vcpkg_cmake_install()
 vcpkg_copy_pdbs()
 vcpkg_fixup_pkgconfig()
 
+vcpkg_copy_tools(TOOL_NAMES aln AUTO_CLEAN)
+
 # Clean
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
+file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/share")
 
 vcpkg_install_copyright(FILE_LIST "${SOURCE_PATH}/LICENSE")
